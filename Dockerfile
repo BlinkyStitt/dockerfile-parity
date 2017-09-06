412b30415fa3213f1f2d7a260b01438e232f1389
Bryan Stitt <bryan@stitthappens.com>
2017-09-05 23:31:50 -0700

    verify checksum for 1.7.0

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,32 @@
+# TODO: debian? does it matter for this?
 FROM bwstitt/ubuntu:16.04
 
 RUN docker-apt-install \
     ca-certificates \
+    torsocks \
     wget
 
-ENTRYPOINT ["parity"]
+ENTRYPOINT ["/entrypoint.sh"]
 
 ENV PARITY_VERSION 1.7.0
 ENV PARITY_PACKAGE parity_${PARITY_VERSION}_amd64
 ENV PARITY_DEB ${PARITY_PACKAGE}.deb
-ENV PARITY_RELEASE http://d1h4xl4cr1h0mo.cloudfront.net/v${PARITY_VERSION}/x86_64-unknown-linux-gnu/${PARITY_DEB}
+ENV PARITY_RELEASE http://parity-downloads-mirror.parity.io/v${PARITY_VERSION}/x86_64-unknown-linux-gnu/${PARITY_DEB}
+ENV PARITY_MD5 266fe720450bf2e3301c62ab69d647f2
 
-RUN wget "$PARITY_RELEASE" \
- && docker-apt-install "./$PARITY_DEB" \
- && rm "$PARITY_DEB"
+# TODO: verify checksum
+RUN set -eux; \
+    \
+    wget "$PARITY_RELEASE"; \
+    md5sum "./$PARITY_DEB" | grep "$PARITY_MD5"; \
+    docker-apt-install "./$PARITY_DEB"; \
+    rm "$PARITY_DEB"
+
+RUN mkdir -p /home/abc/.local/share/io.parity.ethereum; \
+    chown -R abc:abc /etc/tor/torsocks.conf /home/abc/.local/
+
+USER abc
+ENV HOME /home/abc
+VOLUME "/home/abc/.local/share/io.parity.ethereum/"
+
+ADD /entrypoint.sh /
---
+++ entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+# this if will check if the first argument is a flag
+# but only works if all arguments require a hyphenated flag
+# -v; -SL; -f arg; etc will work, but not arg1 arg2
+if [ "${1:0:1}" = '-' ]; then
+    set -- parity "$@"
+fi
+
+# TODO: i keep copypastaing this around. we should include it in the base image
+if [ -n "$TOR_HOSTNAME" ] || [ -n "$TOR_IP" ]; then
+    # torsocks does not support name resolution so we do it here
+    TORSOCKS_CONF_FILE=/etc/tor/torsocks.conf
+    until [ -n "$TOR_IP" ]; do
+        TOR_IP=$(getent hosts "$TOR_HOSTNAME" | cut -f1 -d' ')
+
+        # TODO: max wait?
+        if [ -z "$TOR_IP" ]; then
+            echo "Tor is not yet running at $TOR_HOSTNAME! Sleeping..."
+            sleep 10
+        fi
+    done
+    echo "TorAddress $TOR_IP" > "$TORSOCKS_CONF_FILE"
+    echo "AllowInbound 1" > "$TORSOCKS_CONF_FILE"
+
+    # torify everything
+    export TORSOCKS_CONF_FILE
+    export LD_PRELOAD=/usr/lib/torsocks/libtorsocks.so
+
+    echo "torsocks loaded..."
+fi
+
+# check for the expected command
+if [ "$1" = 'parity' ]; then
+    # TODO: if the config has "rpc-login=monero:changeme", change it automatically
+
+    # TODO: handle adding multiple peers
+    if [ -n "$PARITY_PEER" ]; then
+        echo $PARITY_PEER > "$HOME/reserved_peers"
+
+        set -- "$@" --reserved_peers "$HOME/reserved_peers"
+    fi
+
+    # TODO: optional numactl
+
+    # keep config outside the volume
+    exec "$@"
+fi
+
+# otherwise, don't get in their way
+exec "$@"
